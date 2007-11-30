3140d3fbbbfbf9a97a733e435f38915effdc7214
jenslorenz <jenslorenz>
2007-11-30 00:18:42 +0000

    Added new resources for NativeLang support.

--- SpellChecker/src/resource.h
+++ SpellChecker/src/resource.h
@@ -3,9 +3,6 @@
 // Used by SpellChecker.rc
 //
 #define IDB_SPELLCHECKER                1000
-#define IDC_CHECK_HIDE                  1001
-#define IDC_EDIT1                       1002
-#define IDC_EDIT_RELPATH                1002
 #define IDD_SPELLCHECKER_DLG            1100
 #define IDC_EDIT_REPLACE                1101
 #define IDC_BUTTON_LERN                 1102
@@ -15,12 +12,26 @@
 #define IDC_LIST_SUGGESTION             1106
 #define IDC_STATIC_WORD                 1107
 #define IDC_COMBO_LANG                  1108
+#define IDC_STATIC_ORIG                 1109
+#define IDC_STATIC_REP                  1110
+#define IDC_STATIC_LANG                 1111
 #define IDD_HELP_DLG                    1200
 #define IDC_EMAIL_LINK                  1201
 #define IDC_NPP_PLUGINS_URL             1202
 #define IDC_ASPELL_URL                  1203
 #define IDC_ASPELL_WIN_URL              1204
+#define IDC_STATIC_AUTHOR               1205
+#define IDC_STATIC_MAIL                 1206
+#define IDC_STATIC_INSTAL               1207
+#define IDC_STATIC_NEWPI                1208
+#define IDC_STATIC_ASPELL               1209
+#define IDC_STATIC_MENU                 1210
+#define IDC_STATIC_CHECKER              1211
+#define IDC_STATIC_HELP                 1212
 #define IDD_NOTAVAILABLE_DLG            1300
+#define IDC_EDIT_RELPATH                1301
+#define IDC_STATIC_MISSING              1302
+#define IDC_STATIC_DLLPOS               1303
 
 // Next default values for new objects
 // 
@@ -28,7 +39,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1016
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
