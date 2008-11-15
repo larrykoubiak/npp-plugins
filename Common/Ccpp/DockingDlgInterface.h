9a6195a60cc7c5bdbd57fa3cfcf16826103e9b5c
jenslorenz <jenslorenz>
2008-11-15 18:33:55 +0000

    Merged with my changes.

--- Common/Ccpp/DockingDlgInterface.h
+++ Common/Ccpp/DockingDlgInterface.h
@@ -1,11 +1,11 @@
 /*
-this file is part of Function List Plugin for Notepad++
-Copyright (C)2005 Jens Lorenz <jens.plugin.npp@gmx.de>
+This file is part of Notepad++ - Interface defines
+Copyright (C)2006 Jens Lorenz <jens.plugin.npp@gmx.de>
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either
-version 2 of the License, or (at your option) any later version.
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,16 +14,19 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
-Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
 
 #ifndef DOCKINGDLGINTERFACE_H
 #define DOCKINGDLGINTERFACE_H
 
 #include "StaticDialog.h"
-#include "dockingResource.h"
+#include "Resource.h"
 #include "Docking.h"
 #include <shlwapi.h>
+
+#define UPDATE_CAPTION updateDockingDlg
 
 
 class DockingDlgInterface : public StaticDialog
@@ -37,29 +40,28 @@ public:
 	{
 		StaticDialog::init(hInst, parent);
 		::GetModuleFileName((HMODULE)hInst, _moduleName, MAX_PATH);
-		lstrcpy(_moduleName, PathFindFileName(_moduleName));
+		_tcscpy(_moduleName, PathFindFileName(_moduleName));
 	}
 
     void create(tTbData * data, bool isRTL = false){
 		StaticDialog::create(_dlgID, isRTL);
 		::GetWindowText(_hSelf, _pluginName, sizeof(_pluginName));
 
-        // user information
+        /* user information */
 		data->hClient		= _hSelf;
 		data->pszName		= _pluginName;
 
-		// supported features by plugin
+		/* supported features by plugin */
 		data->uMask			= 0;
 
-		// icons
+		/* icons */
 		//data->hIconBar	= ::LoadIcon(hInst, IDB_CLOSE_DOWN);
 		//data->hIconTab	= ::LoadIcon(hInst, IDB_CLOSE_DOWN);
 
-		// additional info
+		/* additional info */
 		data->pszAddInfo	= NULL;
 
-		_data = data;
-
+		_data				= data;
 	};
 
 	virtual void updateDockingDlg(void) {
@@ -70,19 +72,21 @@ public:
     };
 
 	virtual void display(bool toShow = true) const {
+		extern FuncItem funcItem[];
 		::SendMessage(_hParent, toShow?NPPM_DMMSHOW:NPPM_DMMHIDE, 0, (LPARAM)_hSelf);
+		if (_data != NULL)
+			::SendMessage(_hParent, NPPM_SETMENUITEMCHECK, funcItem[_data->dlgID]._cmdID, (LPARAM)toShow);
 	};
 
-	const TCHAR * getPluginFileName() const {
+	LPCTSTR getPluginFileName() const {
 		return _moduleName;
 	};
 
 protected :
-	virtual BOOL CALLBACK run_dlgProc(UINT message, WPARAM wParam, LPARAM lParam)
+	virtual BOOL CALLBACK run_dlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	{
 		switch (message) 
 		{
-
 			case WM_NOTIFY: 
 			{
 				LPNMHDR	pnmh	= (LPNMHDR)lParam;
@@ -93,22 +97,20 @@ protected :
 					{
 						case DMN_CLOSE:
 						{
-							//::MessageBox(_hSelf, TEXT("Close Dialog"), TEXT("Plugin Message"), MB_OK);
+							extern FuncItem funcItem[];
+							if (_data != NULL)
+								::SendMessage(_hParent, NPPM_SETMENUITEMCHECK, funcItem[_data->dlgID]._cmdID, (LPARAM)FALSE);
 							break;
 						}
 						case DMN_FLOAT:
 						{
-							//::MessageBox(_hSelf, TEXT("Float Dialog"), TEXT("Plugin Message"), MB_OK);
 							_isFloating = true;
 							break;
 						}
 						case DMN_DOCK:
 						{
-							//TCHAR test[256];
-							//wsprintf(test, TEXT("Dock Dialog to %d"), HIWORD(pnmh->code));
-							//::MessageBox(_hSelf, test, TEXT("Plugin Message"), MB_OK);
-							_iDockedPos = HIWORD(pnmh->code);
 							_isFloating = false;
+							_iDockedPos = HIWORD(pnmh->code);
 							break;
 						}
 						default:
@@ -123,13 +125,13 @@ protected :
 		return FALSE;
 	};
 	
-	// Handles
+	/* Handles */
     HWND			_HSource;
 	tTbData*		_data;
-	int				_dlgID;
-	bool            _isFloating;
-	int				_iDockedPos;
-	TCHAR            _moduleName[MAX_PATH];
+	INT				_dlgID;
+	BOOL            _isFloating;
+	INT				_iDockedPos;
+	TCHAR           _moduleName[MAX_PATH];
 	TCHAR			_pluginName[MAX_PATH];
 };
 
