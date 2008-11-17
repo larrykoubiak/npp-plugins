26936aaf7f9cacf6589bcd55bf73da31762c8c95
jenslorenz <jenslorenz>
2008-11-17 21:35:02 +0000

    Initial version.

---
+++ NativeLang/src/NativeLangResource.h
@@ -0,0 +1,40 @@
+/*
+This file is part of Native Language Plugin for Notepad++
+Copyright (C)2008 Jens Lorenz <jens.plugin.npp@gmx.de>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+
+#ifndef NATIVELANG_RC_H
+#define NATIVELANG_RC_H
+
+
+#define    IDD_HELP_DLG	     30700         
+    #define IDC_EMAIL_LINK                  (IDD_HELP_DLG + 1)
+	#define IDC_NPP_PLUGINS_URL				(IDD_HELP_DLG + 2)
+	#define IDC_STATIC_AUTHOR               (IDD_HELP_DLG + 3)
+	#define IDC_STATIC_MAIL                 (IDD_HELP_DLG + 4)
+	#define IDC_STATIC_LIST					(IDD_HELP_DLG + 5)
+	#define IDC_EDIT_LIST					(IDD_HELP_DLG + 6)
+	#define IDC_STATIC_MENU                 (IDD_HELP_DLG + 7)
+	#define IDC_STATIC_HELP                 (IDD_HELP_DLG + 8)
+	#define IDC_STATIC_VERSION              (IDD_HELP_DLG + 9)
+
+#ifndef IDC_STATIC
+#define IDC_STATIC							-1
+#endif
+	
+#endif /* NATIVELANG_RC_H */
